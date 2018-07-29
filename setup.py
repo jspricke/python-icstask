8e08b15be787119a9b7df8dbf36ea88c034b7134
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2018-07-29 14:26:46 +0200

    Version 0.1.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='icstask',
-      version='0.1.6',
+      version='0.1.7',
       description='Python library to convert between Taskwarrior and vObject',
       long_description=open('README.rst').read(),
       author='Jochen Sprickerhof',
